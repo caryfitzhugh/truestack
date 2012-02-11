e3cdd3b8ac6374bdde744492638aa984dd05b6fb
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-02-11 14:23:55 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truestack::Application.config.secret_token = '981f95f680f55acd549c0b67cd926fbbfecf622d6fded04db96a13359e5b7a7e89c18e97777a680b4fcfe49db6483cd91dae076d9ecdf2176931fbed7942a712'
