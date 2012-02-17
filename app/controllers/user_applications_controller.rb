63949eee5f64b8ac526cb5da0bc494b3011e2676
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-02-17 11:01:47 -0500

    continue updates

--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -1,7 +1,10 @@
 class UserApplicationsController < ApplicationController
   def show
+    @user_application = UserApplication.find(params[:id])
+    @user_application.latest_deployment
   end
 
   def index
+    @user_applications = UserApplication.all
   end
 end
