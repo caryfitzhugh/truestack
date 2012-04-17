dcac548497d8c69d1cdbefa0b880bdfddc7b6ea8
Patrick Berkeley <patrickberkeley@gmail.com>
2012-04-17 01:44:14 -0400

    Add stripe gem and config.

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ end
 gem 'jquery-rails'
 gem 'bourbon'
 gem 'simple_form'
+gem 'stripe'
 
 # Collector
 gem 'eventmachine'
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = "mb6xfIC0PpHafb3uqzIeHFD7larGJqw6"
+STRIPE_PUBLIC_KEY = "pk_p73wLuzh9ybATyDUzmeLBiaWXdMXN"
