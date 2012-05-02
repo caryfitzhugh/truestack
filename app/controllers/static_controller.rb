d27441cba8100175fbe7ebfd8945a05ea6e51926
Christopher Sexton <csexton@gmail.com>
2012-05-02 11:35:57 -0400

    Adding example pages from twitter bootstrap to use app layout
    This way you can see how things look as you tweak twitter bootstrap and
    use it as a reference.
    
    http://truestack.dev/bootstrap/components
    http://truestack.dev/bootstrap/scaffolding
    http://truestack.dev/bootstrap/base-css

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
-  def home
-  end
-
-  def about
-  end
+  def home ; end
+  def about ; end
+  def base_css ; end
+  def scaffolding ; end
+  def components; end
 end
