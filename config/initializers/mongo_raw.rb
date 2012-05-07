45a87107d8c8d439d3a943c9199ac730c5b823cc
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2012-05-07 16:22:25 -0400

    Added final touches to JS stored procedures.

--- config/initializers/mongo_raw.rb
+++ config/initializers/mongo_raw.rb
@@ -16,15 +16,19 @@ module MongoRaw
     self.store_procedures!(database)
     database
   end
+
+  # Wrap the args in an anon fuction (hope to make injection a tad harder).
+  # Calls it!
+  #
+  # method_name, arg1, arg2, arg3......
   def self.eval(*args)
     name = args.shift
     arg_names = (1..args.length).map {|i| "a#{i}"}.join(',')
-    #eval_str = "function(#{arg_names}) { return #{name}(#{arg_names});}(#{args.map {|v| v.to_json}.join(',')});"
-    eval_str = "return #{name}(#{args.map {|v| v.to_json}.join(',')});"
-    binding.pry
+    eval_str = "return function(#{arg_names}) { return #{name}(#{arg_names});}(#{args.map {|v| v.to_json}.join(',')});"
     self.db.eval(eval_str)
   end
-  def self.store_procedures!(database)
+
+  def self.store_procedures!(database = MongoRaw.db)
     Dir[Rails.root.join('db','mongo_procedures', "*")].each do |file|
       name = File.basename(file)
       contents = File.read(file)
